fc790bfe496559e64437fa0d943c429434933cd8
gauravgangwarweb <gauravgangwar501@gmail.com>
2025-06-15 21:59:49 +0530

    feat: add List of NGOs button component with animation and styling

---
+++ src/ui/ListOfNgosButton.tsx
@@ -0,0 +1,26 @@
+"use client";
+import Link from "next/link";
+import { motion } from "motion/react"
+import React from "react";
+import { cn } from "@/lib/utils";
+
+interface ListOfNgosButtonProps {
+  className?: string;
+}
+
+const ListOfNgosButton: React.FC<ListOfNgosButtonProps> = ({className}) => {
+  return (
+    <Link className="mt-4" href="/ngo-list" passHref>
+      <motion.span
+        className={cn("bg-blue-400 text-white px-4 py-2 rounded-lg hover:bg-blue-500 text-lg font-semibold", className)}
+        initial= {{ opacity: 0, }}
+        animate= {{ opacity: 1, }}
+        transition={{ duration: 0.5, ease: "easeInOut" }}
+      >
+        List of NGOs
+      </motion.span>
+    </Link>
+  );
+};
+
+export default ListOfNgosButton;
